d6e2bc91cc5c179f335815b39a3ca0813ff26660
Al Cutter <al@google.com>
2023-10-12 17:49:18 +0100

    Use FT for Trusted OS verification (#36)

--- Makefile
+++ Makefile
@@ -18,9 +18,10 @@ BUILD_DATE ?= $(shell /bin/date -u "+%Y-%m-%d %H:%M:%S")
 BUILD_TAGS = linkramsize,linkramstart,linkprintk
 BUILD = ${BUILD_USER}@${BUILD_HOST} on ${BUILD_DATE}
 REV = $(shell git rev-parse --short HEAD 2> /dev/null)
-DEV_LOG_ORIGIN ?= "DEV.armoredwitness.transparency.dev/${USER}"
+LOG_ORIGIN ?= "DEV.armoredwitness.transparency.dev/${USER}"
 GIT_SEMVER_TAG ?= $(shell (git describe --tags --exact-match --match 'v*.*.*' 2>/dev/null || git describe --match 'v*.*.*' --tags 2>/dev/null || git describe --tags 2>/dev/null || echo -n 'v0.0.0+'`git rev-parse HEAD`) | tail -c +2 )
-PUBLIC_KEYS = [\"$(shell test ${OS_PUBLIC_KEY1} && tail -n 1 ${OS_PUBLIC_KEY1})\", \"$(shell test ${OS_PUBLIC_KEY2} && tail -n 1 ${OS_PUBLIC_KEY2})\"]
+LOG_VERIFIER = $(shell test ${LOG_PUBLIC_KEY} && cat ${LOG_PUBLIC_KEY})
+OS_VERIFIERS = [\"$(shell test ${OS_PUBLIC_KEY1} && cat ${OS_PUBLIC_KEY1})\", \"$(shell test ${OS_PUBLIC_KEY2} && cat ${OS_PUBLIC_KEY2})\"]
 
 SHELL = /bin/bash
 
@@ -36,7 +37,12 @@ APP := armored-witness-boot
 CMD := armored-witness-image
 GOENV := GO_EXTLINK_ENABLED=0 CGO_ENABLED=0 GOOS=tamago GOARM=7 GOARCH=arm
 TEXT_START := 0x90010000 # ramStart (defined in imx6/imx6ul/memory.go) + 0x10000
-TAMAGOFLAGS := -tags ${BUILD_TAGS} -trimpath -ldflags "-s -w -T $(TEXT_START) -E _rt0_arm_tamago -R 0x1000 -X 'main.Build=${BUILD}' -X 'main.Revision=${REV}' -X 'main.PublicKeys=${PUBLIC_KEYS}'"
+TAMAGOFLAGS := -tags ${BUILD_TAGS} -trimpath \
+	-ldflags "-s -w -T $(TEXT_START) -E _rt0_arm_tamago -R 0x1000 \
+			  -X 'main.Build=${BUILD}' -X 'main.Revision=${REV}' \
+			  -X 'main.OSLogOrigin=${LOG_ORIGIN}' \
+			  -X 'main.OSLogVerifier=${LOG_VERIFIER}' \
+			  -X 'main.OSManifestVerifiers=${OS_VERIFIERS}'"
 GOFLAGS := -trimpath -ldflags "-s -w"
 
 QEMU ?= qemu-system-arm -machine mcimx6ul-evk -cpu cortex-a7 -m 512M \
@@ -69,7 +75,7 @@ log_initialise:
 	echo "(Re-)initialising log at ${LOG_STORAGE_DIR}"
 	go run github.com/transparency-dev/serverless-log/cmd/integrate@a56a93b5681e5dc231882ac9de435c21cb340846 \
 		--storage_dir=${LOG_STORAGE_DIR} \
-		--origin=${DEV_LOG_ORIGIN} \
+		--origin=${LOG_ORIGIN} \
 		--private_key=${LOG_PRIVATE_KEY} \
 		--public_key=${LOG_PUBLIC_KEY} \
 		--initialise
@@ -93,12 +99,12 @@ log_boot: check_log
 	fi
 	go run github.com/transparency-dev/serverless-log/cmd/sequence@a56a93b5681e5dc231882ac9de435c21cb340846 \
 		--storage_dir=${LOG_STORAGE_DIR} \
-		--origin=${DEV_LOG_ORIGIN} \
+		--origin=${LOG_ORIGIN} \
 		--public_key=${LOG_PUBLIC_KEY} \
 		--entries=${CURDIR}/${APP}_manifest
 	-go run github.com/transparency-dev/serverless-log/cmd/integrate@a56a93b5681e5dc231882ac9de435c21cb340846 \
 		--storage_dir=${LOG_STORAGE_DIR} \
-		--origin=${DEV_LOG_ORIGIN} \
+		--origin=${LOG_ORIGIN} \
 		--private_key=${LOG_PRIVATE_KEY} \
 		--public_key=${LOG_PUBLIC_KEY}
 	@mkdir -p ${LOG_ARTEFACT_DIR}
@@ -108,8 +114,8 @@ log_boot: check_log
 ## log_recovery creates a manifest for a defined version of the armory-ums image, and stores it
 ## in the local dev FT log.
 ## See https://github.com/usbarmory/armory-ums/releases
-log_recovery: ARMORY_UMS_RELEASE=v20201102
-log_recovery: ARMORY_UMS_GIT_TAG="0.0.0-${ARMORY_UMS_RELEASE}" # Workaround for semver format requirement.
+log_recovery: ARMORY_UMS_RELEASE=master
+log_recovery: ARMORY_UMS_GIT_TAG="0.0.0+${ARMORY_UMS_RELEASE}" # Workaround for semver format requirement.
 log_recovery: LOG_STORAGE_DIR=$(DEV_LOG_DIR)/log
 log_recovery: LOG_ARTEFACT_DIR=$(DEV_LOG_DIR)/recovery/$(ARMORY_UMS_GIT_TAG)
 log_recovery: TAMAGO_SEMVER=$(shell ${TAMAGO} version | sed 's/.*go\([0-9]\.[0-9]*\.[0-9]*\).*/\1/')
@@ -135,12 +141,12 @@ log_recovery: check_log
 
 	go run github.com/transparency-dev/serverless-log/cmd/sequence@a56a93b5681e5dc231882ac9de435c21cb340846 \
 		--storage_dir=${LOG_STORAGE_DIR} \
-		--origin=${DEV_LOG_ORIGIN} \
+		--origin=${LOG_ORIGIN} \
 		--public_key=${LOG_PUBLIC_KEY} \
 		--entries=${CURDIR}/armory-ums_manifest
 	-go run github.com/transparency-dev/serverless-log/cmd/integrate@a56a93b5681e5dc231882ac9de435c21cb340846 \
 		--storage_dir=${LOG_STORAGE_DIR} \
-		--origin=${DEV_LOG_ORIGIN} \
+		--origin=${LOG_ORIGIN} \
 		--private_key=${LOG_PRIVATE_KEY} \
 		--public_key=${LOG_PUBLIC_KEY}
 	@mkdir -p ${LOG_ARTEFACT_DIR}
--- main.go
+++ main.go
@@ -15,29 +15,41 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
 
 	usbarmory "github.com/usbarmory/tamago/board/usbarmory/mk2"
 	"github.com/usbarmory/tamago/dma"
 	"github.com/usbarmory/tamago/soc/nxp/imx6ul"
+	"github.com/usbarmory/tamago/soc/nxp/usdhc"
+	"golang.org/x/mod/sumdb/note"
 
 	"github.com/usbarmory/armory-boot/exec"
 
 	"github.com/transparency-dev/armored-witness-boot/config"
-	"github.com/transparency-dev/armored-witness-boot/crypto"
-	"github.com/transparency-dev/armored-witness-boot/storage"
+	"github.com/transparency-dev/armored-witness-common/release/firmware"
+	"github.com/transparency-dev/armored-witness-common/release/firmware/ftlog"
 )
 
-// Quorum defines the number of required authentication signatures for
-// unikernel loading.
-const Quorum = 2
+const (
+	// Quorum defines the number of required authentication signatures for
+	// unikernel loading.
+	Quorum = 2
+
+	expectedBlockSize = 512
+	osConfBlock       = 0x5000
+)
 
 var (
 	Build    string
 	Revision string
 
-	PublicKeys string
+	OSLogOrigin         string
+	OSLogVerifier       string
+	OSManifestVerifiers string
 )
 
 // DMA region for target kernel boot
@@ -66,47 +78,106 @@ func preLaunch() {
 	usbarmory.LED("white", false)
 }
 
+// read reads the firmware bundle for the trusted OS from internal storage, the
+// OS proof bundle is *not* verified by this function.
+func read(card *usdhc.USDHC) (fw *firmware.Bundle, err error) {
+	blockSize := card.Info().BlockSize
+	if blockSize != expectedBlockSize {
+		return nil, fmt.Errorf("h/w invariant error - got MMC blocksize %d, want %d", blockSize, expectedBlockSize)
+	}
+
+	buf, err := card.Read(osConfBlock*expectedBlockSize, config.MaxLength)
+	if err != nil {
+		return nil, err
+	}
+
+	conf := &config.Config{}
+	if err = conf.Decode(buf); err != nil {
+		return nil, err
+	}
+
+	fw = &firmware.Bundle{
+		Checkpoint:     conf.Bundle.Checkpoint,
+		Index:          conf.Bundle.LogIndex,
+		InclusionProof: conf.Bundle.InclusionProof,
+		Manifest:       conf.Bundle.Manifest,
+	}
+
+	fw.Firmware, err = card.Read(conf.Offset, conf.Size)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read firmware: %v", err)
+	}
+
+	return fw, nil
+}
+
 func main() {
 	card := usbarmory.MMC
 
 	usbarmory.LED("blue", false)
 	usbarmory.LED("white", false)
 
-	if len(PublicKeys) == 0 {
-		panic("missing public keys, aborting")
+	if len(OSManifestVerifiers) == 0 {
+		panic("armored-witness-boot: missing public keys, aborting")
 	}
 
 	if err := card.Detect(); err != nil {
-		panic(fmt.Sprintf("boot media error, %v\n", err))
+		panic(fmt.Sprintf("armored-witness-boot: boot media error, %v\n", err))
 	}
 
 	usbarmory.LED("blue", true)
 
-	log.Printf("armored-witness-boot: loading configuration at USDHC%d@%d\n", card.Index, config.Offset)
-
-	conf, err := storage.Configuration(card, config.Offset, config.MaxLength)
-
+	log.Printf("armored-witness-boot: loading configuration & kernel at USDHC%d@%d\n", card.Index, config.Offset)
+	os, err := read(card)
 	if err != nil {
-		panic(fmt.Sprintf("configuration read error, %v\n", err))
+		panic(fmt.Sprintf("armored-witness-boot: Failed to read OS firmware bundle: %v", err))
 	}
 
-	kernel, err := card.Read(conf.Offset, conf.Size)
-
+	logVerifier, err := note.NewVerifier(OSLogVerifier)
 	if err != nil {
-		panic(fmt.Sprintf("kernel read error, %v\n", err))
+		panic(fmt.Sprintf("armored-witness-boot: Invalid OSLogVerifier: %v", err))
+	}
+	log.Printf("armored-witness-boot: log verifier: %s", OSLogVerifier)
+
+	manifestVerifiers, err := manifestVerifiers()
+	if err != nil {
+		panic(fmt.Sprintf("armored-witness-boot: Invalid OSManifestVerifiers: %v", err))
 	}
 
-	if err = crypto.VerifyConfig(*conf, kernel, PublicKeys, Quorum); err != nil {
-		panic(fmt.Sprintf("configuration verification error, %v\n", err))
+	bv := &firmware.BundleVerifier{
+		LogOrigin:         OSLogOrigin,
+		LogVerifer:        logVerifier,
+		ManifestVerifiers: manifestVerifiers,
+	}
+	if err := bv.Verify(*os); err != nil {
+		panic(fmt.Sprintf("armored-witness-boot: kernel verification error, %v", err))
+	}
+
+	// For reference, this is how we'd fall back to verifying signatures only.
+	if false {
+		n, err := note.Open(os.Manifest, note.VerifierList(manifestVerifiers...))
+		if err != nil {
+			panic(fmt.Sprintf("armored-witness-boot: kernel verification error, Open: %v", err))
+		}
+		relManifest := ftlog.FirmwareRelease{}
+		if err := json.Unmarshal([]byte(n.Text), &relManifest); err != nil {
+			panic(fmt.Sprintf("armored-witness-boot: kernel verification error, invalid manifest: %v", err))
+		}
+		if got, want := len(n.Sigs), len(manifestVerifiers); got < want {
+			panic(fmt.Sprintf("armored-witness-boot: kernel verification error, quorum not met (%d < %d)", got, want))
+		}
+		if fwHash, mHash := sha256.Sum256(os.Firmware), relManifest.FirmwareDigestSha256; !bytes.Equal(fwHash[:], mHash) {
+			panic("armored-witness-boot: kernel verification error, firmware hash != manifest hash")
+		}
 	}
 
 	usbarmory.LED("white", true)
 
-	log.Printf("armored-witness-boot: loaded kernel off:%x size:%d", conf.Offset, conf.Size)
+	log.Print("armored-witness-boot: verified kernel")
 
 	image := &exec.ELFImage{
 		Region: mem,
-		ELF:    kernel,
+		ELF:    os.Firmware,
 	}
 
 	if err = image.Load(); err != nil {
@@ -116,6 +187,27 @@ func main() {
 	log.Printf("armored-witness-boot: starting kernel@%.8x\n", image.Entry())
 
 	if err = image.Boot(preLaunch); err != nil {
-		panic(fmt.Sprintf("load error, %v\n", err))
+		panic(fmt.Sprintf("armored-witness-boot: load error, %v\n", err))
 	}
+}
+
+func manifestVerifiers() ([]note.Verifier, error) {
+	var manifestKeys []string
+	if err := json.Unmarshal([]byte(OSManifestVerifiers), &manifestKeys); err != nil {
+		return nil, fmt.Errorf("invalid OSManifestVerifiers format: %v", err)
+	}
+	manifestVerifiers := make([]note.Verifier, 0, len(manifestKeys))
+	for _, v := range manifestKeys {
+		mv, err := note.NewVerifier(v)
+		if err != nil {
+			return nil, fmt.Errorf("invalid OSManifestVerifier %q: %v", v, err)
+		}
+		manifestVerifiers = append(manifestVerifiers, mv)
+		log.Printf("armored-witness-boot: kernel verifier: %v", v)
+	}
+	if l := len(manifestVerifiers); l != Quorum {
+		return nil, fmt.Errorf("insufficient number of kernel manifest verifiers %d, need quorum of %d", l, Quorum)
+	}
+
+	return manifestVerifiers, nil
 }
